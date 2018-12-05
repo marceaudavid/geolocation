f9d566038c7e03886da2aca7d9edc97278d3bb62
marceaudavid <marceau.david@ynov.com>
2018-12-05 01:08:38 +0100

    redesign et plusieurs fix

--- js/script.js
+++ js/script.js
@@ -1,29 +1,33 @@
 var geolocate = document.getElementById('geolocate');
+var track = document.getElementById('track');
 var coords = {
     time: 0,
     lat: 0,
     long: 0
 };
+var menu = document.getElementById('menu');
+var panel = document.getElementById('panel');
+
+var i;
 
 if (localStorage.getItem(0)) {
-    var i = localStorage.getItem(0)
+    i = localStorage.getItem(0)
 } else {
-    var i = 0;
+    i = 0;
 }
 
 window.onload = () => {
-    for (let a = 1; a < localStorage.length; a++) {
-        showData(a);
+    for (let i = 1; i < localStorage.length; i++) {
+        showData(i);
     }
 }
 
 window.onbeforeunload = () => {
-    if (localStorage.length == i) {
+    if (localStorage.length == i && localStorage.length != 0) {
         localStorage.setItem(0, i);
     }
 }
 
-var popup = document.getElementById('pop-close');
 // create the map
 var map = L.map('map');
 map.setView([coords.lat, coords.long], 3);
@@ -38,7 +42,6 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
 function getCurrentPosition() {
     if (navigator.geolocation) {
         // return a coordinate object to the function specified in the parameter.
-        popup.parentElement.style.visibility = "hidden";
         navigator.geolocation.getCurrentPosition(showCurrentPosition);
     } else {
         console.log("Sorry, Geolocation is not enabled in your browser");
@@ -48,21 +51,25 @@ function getCurrentPosition() {
 function watchCurrentPosition() {
     if (navigator.geolocation) {
         // return a coordinate object to the function specified in the parameter.
-        popup.parentElement.style.visibility = "hidden";
         navigator.geolocation.watchPosition(showCurrentPosition);
     } else {
         console.log("Sorry, Geolocation is not enabled in your browser");
     }
 }
 
+function setMapPosition(lat, long) {
+    map.setView([lat, long], 13);
+    var marker = L.marker([lat, long]).addTo(map);
+    marker.bindPopup("<b>You are here !</b>").openPopup();
+}
 
 function showCurrentPosition(position) {
+    var date = new Date();
+    coords.time = date.toLocaleTimeString();
     coords.lat = position.coords.latitude;
     coords.long = position.coords.longitude;
     console.log("Latitude: " + coords.lat + " Longitude: " + coords.long);
-    map.setView([coords.lat, coords.long], 13)
-    var marker = L.marker([coords.lat, coords.long]).addTo(map);
-    marker.bindPopup("<b>You are here !</b>").openPopup();
+    setMapPosition(coords.lat, coords.long);
     var coordsJSON = JSON.stringify(coords);
     i++;
     localStorage.setItem(i, coordsJSON);
@@ -71,17 +78,16 @@ function showCurrentPosition(position) {
 
 function showData(index) {
     var data = JSON.parse(localStorage.getItem(index));
-    console.log(data);
-    var table = document.getElementById('table');
-    var row = table.insertRow(index);
+    var table = document.getElementById('table').getElementsByTagName('tbody')[0];
+    var row = table.insertRow(index - 1);
     var id = row.insertCell(0);
     var time = row.insertCell(1);
     var lat = row.insertCell(2);
     var long = row.insertCell(3);
     id.innerHTML = index;
     time.innerHTML = data.time;
-    lat.innerHTML = data.lat.toFixed(5);
-    long.innerHTML = data.long.toFixed(5);
+    lat.innerHTML = data.lat.toFixed(5)
+    long.innerHTML = data.long
 }
 
 geolocate.addEventListener('click', () => {
@@ -90,10 +96,6 @@ geolocate.addEventListener('click', () => {
 
 track.addEventListener('click', () => {
     watchCurrentPosition();
-})
-
-popup.addEventListener('click', () => {
-    popup.parentElement.style.visibility = "hidden";
 })
 
 menu.addEventListener('click', () => {
